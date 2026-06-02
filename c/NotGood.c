fde7700eb07da39fffc785c2ba2992a0ffbfe92f
akasako <redfeatherakabane@gmail.com>
2026-06-03 00:05:24 +0900

    add cpp, c, java, js, python, ruby and shell script

---
+++ c/NotGood.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    for(int i = 1; i <= 100; i++) {
+        ((i % 3 == 0) && (i % 5 == 0)) ? printf("FizzBuzz\n") : (i % 3 == 0) ? printf("Fizz\n") : (i % 5 == 0) ? printf("Buzz\n") : printf("%d\n", i);
+    }
+    return 0;
+}
